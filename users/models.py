ecb7efb97313fb117d2d6797ee0adc6b3e408d57
james <jimmindungu3@gmail.com>
2024-12-04 12:17:13 +0300

    add user_id to users model

--- users/models.py
+++ users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=20, unique=True, primary_key=True)
-    email = models.EmailField()
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=12, unique=True)
     registration_date = models.DateField(auto_now_add=True)
 
